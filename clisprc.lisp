fbfd1003777c26be1e9426658bb4b50f167641cf
Edelcides Gonçalves <eatg75@gmail.com>
2016-02-16 03:13:27 +0000

    Added some `run config' files for some CL implementations, also changed some `tmux (1)' keybindings to conform to `emacs (1)' keybindings.

---
+++ clisprc.lisp
@@ -0,0 +1,5 @@
+(setf custom:*clhs-root-default* "~/slnc/local/share/doc/lsp/hypersec/"
+      editor-name "emacs"
+      *print-case* :downcase        ; Show downcase package name.
+      *load-verbose* nil	    ; Disable verbosity when loading files.
+      custom:*load-compiling*) 	    ; Compile loaded files at startup.
