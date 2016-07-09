cfae563d7035ac14991b06a07c373f61b4fffdde
Edelcides Gonçalves <eatg75@gmail.com>
2016-07-09 11:08:42 +0100

    Removed CLisp rc file.

--- lsp/clisprc.lisp
+++
@@ -1,8 +0,0 @@
-(setf
- *print-case* :downcase           ; Show downcase package name.
- *load-verbose* nil)              ; Disable verbosity when loading files.
-
-#-quicklisp
-(let ((quicklisp-init (merge-pathnames "slnc/local/share/lisp/setup.lisp" (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
