1bc4af322018fc7186f9df73404854cf32899fa2
Edelcides Gonçalves <eatg75@gmail.com>
2016-09-18 05:11:40 +0100

    Improved configurations.

---
+++ textadept/init.lua
@@ -0,0 +1,8 @@
+ui.tabs = true
+textadept.editing.strip_trailing_spaces = true
+buffer.use_tabs = false
+
+if not CURSES then
+   ui.set_theme('base16-solarized-light',
+		{font = 'Monospace', fontsize = 10})
+end
